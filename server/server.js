dfffb70287dbddec19e8aef6386732cf56ab65ea
Gurudatta Mulage <gurudattamulage@gmail.com>
2026-07-01 19:14:06 +0530

    Connect MongoDB with Express server

--- server/server.js
+++ server/server.js
@@ -2,16 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
+// Connect to MongoDB
+connectDB();
+
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Test Route
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
@@ -19,6 +25,7 @@ app.get("/", (req, res) => {
   });
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
